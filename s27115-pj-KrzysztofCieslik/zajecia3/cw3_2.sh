4a1e8eb107baa632e354d14d2d87ff12a83dad15
s27115-pj <s27115@pjwstk.edu.pl>
2023-05-22 20:46:50 +0200

    remove echo from cw3_2.sh

--- s27115-pj-KrzysztofCieslik/zajecia3/cw3_2.sh
+++ s27115-pj-KrzysztofCieslik/zajecia3/cw3_2.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+#limitem jest że return jest intermpertowany jako exit code i jest on 
+#8 bitowy co powoduje ograniczenie wartości do zakresu od 0 do 255 
+#wyniki wieksze od 255 są nie określone
+#informacja na ten temat znajduje sie w man exit
+#aby to ominąć należało by wykorzystać inny mechanizm przekazania
+#zmiennej poza funkcję np.przypisane go do zmiennej dostępnej w 
+#funkcji i poza nią lub np. wykorzystać strumień wyjściowy  
+
 
 fibonacci() {
-    if [ "$1" -le 1 ]; then
-        echo "$1"
+    if [ "$1" -le 0 ]; then
+        return "$1"
+    elif [ "$1" -eq 1 ]; then
+        return 1
     else
-        local a=$(fibonacci "$(( $1 - 1 ))")
-        local b=$(fibonacci "$(( $1 - 2 ))")
-        echo "$(( $a + $b ))"
+        local a
+        local b
+        fibonacci "$(( $1 - 1 ))"
+        a=$?
+        fibonacci "$(( $1 - 2 ))"
+        b=$?
+        return "$(( $a + $b ))"
     fi
 }
 
@@ -21,6 +35,7 @@ if ! [[ "$1" =~ ^[0-9]+$ ]]; then
 fi
 
 n=$1
-for i in $(seq 0 $(( n - 1 ))); do
-    echo $(fibonacci "$i")
+for i in $(seq 1 $n); do
+    fibonacci "$i"
+    echo "$?"
 done
